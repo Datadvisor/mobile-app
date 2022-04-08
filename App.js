52e51368159e3229b8de2ba7d8b917c92ee95876
pr0m3th3usEx <thomas1.michel@epitech.eu>
2022-04-08 13:32:15 +0200

    Test textfield

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Button, Card, Text, View } from 'react-native-ui-lib';
+import { Button, Incubator, View } from 'react-native-ui-lib';
 
 export default function App() {
   return (
     <View flex padding-page>
-      <Text heading marginB-s4>
-        My Screen
-      </Text>
-      <Card height={100} center padding-card marginB-s4>
-        <Text body>This is an example card </Text>
-      </Card>
+      <Incubator.TextField
+        placeholder={'E-mail'}
+        floatingPlaceholder
+        onChangeText={() => console.log('changed')}
+        enableErrors
+        validate={['required', 'email']}
+        validationMessage={['Field is required', 'Email is invalid']}
+        showCharCounter
+        maxLength={30}
+      />
 
       <Button label="Button" body bg-primaryColor square></Button>
     </View>
