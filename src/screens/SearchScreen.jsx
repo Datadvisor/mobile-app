7cafd919d319fe3a981d82dcda229b303b14554b
pr0m3th3usEx <thomas1.michel@epitech.eu>
2022-05-29 22:36:40 +0200

    add dark status bar

--- src/screens/SearchScreen.jsx
+++ src/screens/SearchScreen.jsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import { Button, StatusBar, View } from 'native-base';
 
-export default function HomeScreen() {
+export default function SearchScreen() {
   return (
     <>
       <StatusBar bg="#3700B3" barStyle="light-content" />
       <View flex={1} w="100%" h="100%" justifyContent="center">
-        <Button variant="primary">Home</Button>
+        <Button variant="primary">Search</Button>
       </View>
     </>
   );
