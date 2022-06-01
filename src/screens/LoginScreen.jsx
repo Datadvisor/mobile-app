21426fec10faef76ba84ce68fbdf53cc82f42474
verofoxy <veronia.mikhail@epitech.eu>
2022-06-01 23:44:12 +0200

    feat : login done interact with api done

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -6,66 +6,84 @@ import { validateLoginInputs } from '../utils/forms/validation';
 import { useLoginMutation } from '../services/auth';
 
 export default function LoginScreen({ navigation }) {
-  const [inputErrors, setInputErrors] = React.useState({});
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const [login, { isLoading, isSuccess, isError, error }] = useLoginMutation();
+const [inputErrors, setInputErrors] = React.useState({});
+const [email, setEmail] = React.useState('');
+const [password, setPassword] = React.useState('');
+const [login, { isLoading, isSuccess, isError, error }] = useLoginMutation();
 
-  const onSubmit = React.useCallback(() => {
-    const errors = validateLoginInputs(email, password);
-    //check mail and password
-    if (Object.values(errors).some((e) => e)) {
-      setInputErrors(errors);
-      return;
-    }
-    // Call API to Login user
-    login({ email, password });
-  }, [email, password, login]);
+React.useEffect(() => {
+  if (!isLoading && isSuccess && navigation) {
+    navigation.replace('Main');
+  }
+}, [navigation, isSuccess, isLoading]);
 
-  return (
-    <ScrollView>
-      <AuthLayout title="Sign In to your account" subtitle="Clean your data across the web">
-        <VStack w="100%" space="24px">
-          <LabelWrappedInput label="E-mail" error={inputErrors.email}>
-            <Input
-              onChangeText={(text) => {
-                setInputErrors({ ...inputErrors, email: undefined });
-                setEmail(text.toLowerCase().trim());
-              }}
-              placeholder="Ex: my@data.visor"
-              value={email}
-            />
-          </LabelWrappedInput>
+React.useEffect(() => {
+  if (isError) { 
+    console.log(error, "error");
+  }
+}, [isError, error]);
 
-          <LabelWrappedInput label="Password" error={inputErrors.password}>
-            <Input
-              type="password"
-              onChangeText={(pwd) => {
-                setInputErrors({ ...inputErrors, password: undefined });
-                setPassword(pwd);
-              }}
-              placeholder="Min. 8 characters"
-              value={password}
-            />
-          </LabelWrappedInput>
+const onSubmit = React.useCallback(() => {
+  const errors = validateLoginInputs(email, password);
+  //check mail and password
+  if (Object.values(errors).some((e) => e)) {
+    setInputErrors(errors);
+    return;
+  }
+  // Call API to Login user
+  login({ email, password });
+}, [email, password, login]);
 
-          <Button variant="primary" w="100%" onPress={onSubmit} isLoading={isLoading}>
-            Sign In
-          </Button>
-        </VStack>
-        <Text>
-          Don’t have an account yet ?{' '}
-          <Text
-            color="blue"
-            fontWeight="bold"
-            onPress={() => {
-              navigation.replace('Register');
+return (
+  <ScrollView>
+    <AuthLayout title="Sign In to your account" subtitle="Clean your data across the web">
+      <VStack w="100%" space="24px">
+        <LabelWrappedInput label="E-mail" error={inputErrors.email}>
+          <Input
+            onChangeText={(text) => {
+              setInputErrors({ ...inputErrors, email: undefined });
+              setEmail(text.toLowerCase().trim());
             }}
-          >
-            Sign up
+            placeholder="Ex: my@data.visor"
+            value={email}
+          />
+        </LabelWrappedInput>
+
+        <LabelWrappedInput label="Password" error={inputErrors.password}>
+          <Input
+            type="password"
+            onChangeText={(pwd) => {
+              setInputErrors({ ...inputErrors, password: undefined });
+              setPassword(pwd);
+            }}
+            placeholder="Min. 8 characters"
+            value={password}
+          />
+        </LabelWrappedInput>
+
+        <Button variant="primary" w="100%" onPress={onSubmit} isLoading={isLoading}>
+          Sign In
+        </Button>
+
+        {error && 'data' in error && (
+          <Text w="100%" textAlign="center" color="red.500">
+            {error.data.error.message}
           </Text>
+        )}
+      </VStack>
+      <Text>
+        Don’t have an account yet ?{' '}
+        <Text
+          color="blue"
+          fontWeight="bold"
+          onPress={() => {
+            navigation.replace('Register');
+          }}
+        >
+          Sign up
         </Text>
-      </AuthLayout>
-    </ScrollView>
-  );
+      </Text>
+    </AuthLayout>
+  </ScrollView>
+);
 }
